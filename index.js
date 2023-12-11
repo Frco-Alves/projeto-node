bf122a626e5f602d55cbd01a631ce9002908b418
Frco-Alves <alvesszoo750@gmail.com>
2023-12-11 17:36:18 -0300

    projeto-node

---
+++ index.js
@@ -0,0 +1,72 @@
+const express = require('express');
+const uuid = require('uuid')
+
+const port = 3000
+const app = express()
+app.use(express.json())
+
+
+const users = []
+
+const checkUserId = (request, response, next) => {
+   const { id } = request.params
+
+   const index = users.findIndex(user => user.id === id)
+
+   if (index < 0) {
+      return response.status(404).json({ Erro: "User not found" })
+   }
+
+   request.userIndex = index
+   request.userId = id
+
+   next()
+}
+
+
+app.get('/users', (request, response) => {
+   return response.json(users)
+})
+
+app.post('/users', (request, response) => {
+   try {
+      const { name, age } = request.body
+
+      if(age < 18) throw new Error("Only allowed users over 18 year old")
+
+      const user = { id: uuid.v4(), name, age }
+
+      users.push(user)
+
+      return response.status(201).json(user)
+   } catch(err){
+      return response.status(400).json({error:err.message})
+   }
+   
+})
+
+app.put('/users/:id', checkUserId, (request, response) => {
+
+   const { name, age } = request.body
+   const index = request.userIndex
+   const id = request.userId
+
+   const updatedUser = { id, name, age }
+
+   users[index] = updatedUser
+
+   return response.json(updatedUser)
+})
+
+app.delete('/users/:id', checkUserId, (request, response) => {
+
+      const index = request.userIndex
+
+      users.splice(index, 1)
+   
+      return response.status(204).json()
+})
+
+app.listen(port, () => {
+   console.log(`🚀 Server started on port ${port} 👍`)
+});
