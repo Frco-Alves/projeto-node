5039bb105202aab62e4f4c0848b396f1984ebc84
Frco-Alves <alvesszoo750@gmail.com>
2024-01-22 10:55:28 -0300

    projeto-node e react

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
+
 const express = require('express');
 const uuid = require('uuid')
+const cors = require('cors')
 
-const port = 3000
-const app = express()
-app.use(express.json())
+//import express from "express";
+//import { v4 } from "uuid";
+//import cors from "cors"
+
+const port = 3001;
+const app = express();
+app.use(express.json());
+app.use(cors());
 
 
-const users = []
+const users = [];
 
 const checkUserId = (request, response, next) => {
-   const { id } = request.params
+   const { id } = request.params;
 
-   const index = users.findIndex(user => user.id === id)
+   const index = users.findIndex((user) => user.id === id);
 
    if (index < 0) {
-      return response.status(404).json({ Erro: "User not found" })
+      return response.status(404).json({ Erro: "User not found" });
    }
 
-   request.userIndex = index
-   request.userId = id
+   request.userIndex = index;
+   request.userId = id;
 
-   next()
+   next();
 }
 
 
-app.get('/users', (request, response) => {
-   return response.json(users)
-})
+app.get("/order", (request, response) => {
+   return response.json(users);
+});
 
-app.post('/users', (request, response) => {
-   try {
-      const { name, age } = request.body
+app.post("/order", (request, response) => {
+   const { name, age } = request.body;
+   const user = { id: uuid.v4(), name, age };
 
-      if(age < 18) throw new Error("Only allowed users over 18 year old")
+   users.push(user)
 
-      const user = { id: uuid.v4(), name, age }
+   return response.status(201).json(user)
 
-      users.push(user)
+});
 
-      return response.status(201).json(user)
-   } catch(err){
-      return response.status(400).json({error:err.message})
-   }
-   
-})
-
-app.put('/users/:id', checkUserId, (request, response) => {
+app.put("/order/:id", checkUserId, (request, response) => {
 
    const { name, age } = request.body
    const index = request.userIndex
@@ -58,13 +58,13 @@ app.put('/users/:id', checkUserId, (request, response) => {
    return response.json(updatedUser)
 })
 
-app.delete('/users/:id', checkUserId, (request, response) => {
+app.delete("/order/:id", checkUserId, (request, response) => {
 
-      const index = request.userIndex
+   const index = request.userIndex
 
-      users.splice(index, 1)
-   
-      return response.status(204).json()
+   users.splice(index, 1)
+
+   return response.status(204).json()
 })
 
 app.listen(port, () => {
